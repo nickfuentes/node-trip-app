edc318ecfcd9dc5f43a92ec7c3d2d539c6f9d9ad
Full Name <nickfuentes24@gmail.com>
2019-08-06 15:03:14 -0500

    Created a server side node trip app.

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express")
+const app = express()
+const mustacheExpress = require("mustache-express")
+
+const Trip = require("./models/trip")
+
+app.use(express.urlencoded({ extended: false }))
+
+app.engine("mustache", mustacheExpress())
+app.set("views", "./views")
+app.set("view engine", "mustache")
+
+let trips = []
+
+// Shows all the trips created
+app.get("/trips", (req, res) => {
+    res.render('trips', { trips: trips })
+})
+
+// Show the FORM to create a trip
+app.get("/create-trip", (req, res) => {
+    res.render("create-trip")
+})
+
+// POST the trip to the array
+app.post('/create-trip', (req, res) => {
+    let tripTitle = req.body.tripTitle
+    let tripImage = req.body.tripImage
+    let dateDEP = req.body.dateDEP
+    let dateRET = req.body.dateRET
+
+    // console.log(tripTitle)
+    // console.log(tripImage)
+    // console.log(dateDEP)
+    // console.log(dateRET)
+
+    let trip = new Trip(tripTitle, tripImage, dateDEP, dateRET)
+
+    trips.push(trip)
+    // console.log(trips)
+
+    res.redirect("/trips")
+})
+
+app.post("/delete-trip", (req, res) => {
+    let tripID = req.body.tripID
+    // console.log("Trying to Delete Trip")
+    // console.log(tripID)
+    // console.log(trips)
+
+    function removeTrip(trips, tripID) {
+
+        return trips.filter(function (trip) {
+            return trip.title != tripID;
+        });
+
+    }
+
+    trips = removeTrip(trips, tripID);
+
+    res.redirect("/trips")
+})
+
+
+app.listen(3000, () => {
+    console.log("The server is running Nick....")
+})
---
+++ models/trip.js
@@ -0,0 +1,12 @@
+class Trip {
+    constructor(tripTitle, tripImage, dateDEP, dateRET) {
+        this.title = tripTitle
+        this.image = tripImage
+        this.dateDEP = dateDEP
+        this.dateRET = dateRET
+    }
+}
+
+
+
+module.exports = Trip
